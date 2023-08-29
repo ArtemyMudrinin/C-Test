e391d425e3f5fc2c86c46ba95367cadac543b1ee
ArtemyMudrinin <103022037+ArtemyMudrinin@users.noreply.github.com>
2023-08-29 21:28:40 +0300

    New

---
+++ WebApplication1/WebApplication1/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string middleName { get; set; }
+        public string workplace { get; set; }
+    }
+}
